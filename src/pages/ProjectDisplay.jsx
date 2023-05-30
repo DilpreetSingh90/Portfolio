c2a62f86c9a3eaf701ac0bf5fe6c4ea51b175f82
dilpreet90 <mrdilpreet90@gmail.com>
2023-05-30 22:38:34 +0530

    latest version

--- src/pages/ProjectDisplay.jsx
+++ src/pages/ProjectDisplay.jsx
@@ -11,7 +11,7 @@ const ProjectDisplay = () => {
     return (
         <div className="project">
         <h1> {project.name}</h1>
-        <img src={project.image} alt='image'/>
+        <img src={project.image} alt=''/>
         <p>
             <b>Skills:</b> {project.skills}
         </p>
