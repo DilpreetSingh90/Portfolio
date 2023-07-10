e524edd8f3a0acb8e36be667bfb5f84c57facf47
dilpreet90 <mrdilpreet90@gmail.com>
2023-07-10 18:52:24 +0530

    shopster

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -2,30 +2,40 @@ import Keeper from '../assets/Keeper.jpg';
 import Portfolio from '../assets/Portfolio.jpg';
 import Chatapp from '../assets/Chatapp.jpg';
 import Cartoonify from '../assets/Cartoonify.jpg';
+import Shopster from '../assets/Shopster.jpg';
 
 export const ProjectList = [
     {
-    name: "Google Notes Replica",
-    image: Keeper,
-    skills: "React,MongoDB,HTML,CSS",
-    link: "https://github.com/DilpreetSingh90/Keeper-app"
+        name: "Keeper App - To Do List",
+        image: Keeper,
+        skills: "React, MongoDB, HTML, CSS",
+        url: "https://keeper-app-client.netlify.app/",
+        link: "https://github.com/DilpreetSingh90/Keeper-app",
     },
     {
-    name: "Portfolio",
-    image: Portfolio,
-    skills: "React,HTML,CSS",
-    link: "https://github.com/DilpreetSingh90/Portfolio"
+        name: "Portfolio",
+        image: Portfolio,
+        skills: "React, HTML, CSS",
+        url: "https://dilpreetportfolio.netlify.app",
+        link: "https://github.com/DilpreetSingh90/Portfolio",
     },
     {
-    name: 'Chat App',
-    image: Chatapp,
-    skills: "React Native,Firebase",
-    link: "https://github.com/DilpreetSingh90/chat-app"
+        name: 'NXTChat - Realtime Chat App',
+        image: Chatapp,
+        skills: "React Native, Firebase",
+        link: "https://github.com/DilpreetSingh90/chat-app",
     },
     {
-    name: 'Cartoonify Clone',
-    image: Cartoonify,
-    skills: "React.js,Tailwind,MongoDB,Node.js,Express.js",
-    link: "https://github.com/DilpreetSingh90/Cartoonify-clone"
+        name: 'Cartoonify Clone',
+        image: Cartoonify,
+        skills: "React, Tailwind, MongoDB, Node, Express",
+        link: "https://github.com/DilpreetSingh90/Cartoonify-clone",
+    },
+    {
+        name: "Shopster - An Ecommerce App",
+        image: Shopster,
+        skills: "Next.js, Tailwind",
+        url: "https://shopster-blue.vercel.app/",
+        link: "https://github.com/DilpreetSingh90/Shopster-ecommerce-app",
     }                     
 ];
--- src/pages/ProjectDisplay.jsx
+++ src/pages/ProjectDisplay.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from "react-router-dom";
 import { ProjectList } from "../helpers/ProjectList";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import PreviewIcon from '@mui/icons-material/Preview';
 import "../styles/ProjectDisplay.css";
 
 const ProjectDisplay = () => {
@@ -10,12 +11,15 @@ const ProjectDisplay = () => {
     const project = ProjectList[id];
     return (
         <div className="project">
-        <h1> {project.name}</h1>
-        <img src={project.image} alt=''/>
-        <p>
-            <b>Skills:</b> {project.skills}
-        </p>
-        <a href={project.link}><GitHubIcon /></a>
+            <h1> {project.name}</h1>
+            <img src={project.image} alt=''/>
+            <p>
+                <b>Skills:</b> {project.skills}
+            </p>
+            <div>
+                {project.url && <a target='__blank' href={project.url}><PreviewIcon /></a>}
+                <a target='__blank' href={project.link}><GitHubIcon /></a>
+            </div>
         </div>
     );
 }
