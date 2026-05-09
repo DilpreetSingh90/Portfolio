553a174cbcebed8e0a3de59836f27fc5099f0d29
Dilpreet Singh <mrdilpreet90@gmail.com>
2026-05-09 16:26:36 +0530

    fix errors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import InstagramIcon from "@mui/icons-material/Instagram";
-import FacebookIcon from "@mui/icons-material/Facebook";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import "../styles/Footer.css";
